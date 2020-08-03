49e50158e41afec7acfc795a85646a35e2ccb3c0
Josh Mu <mu@joshmu.com>
2020-08-03 16:41:18 +1000

    :sparkles: retry when fail, multiple messages

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+const MESSAGES_TO_SEND = [
+  {
+    message: 'first message ⚡️',
+  },
+  {
+    message: './attachment.gif',
+    isFile: true,
+  },
+  {
+    message: 'third message 👾',
+  },
+]
+
+/**
+ * @file Main File to send bulk imessages
+ *
+ * Currently configured to send 3 messages per contact in the contacts.txt file
+ *
+ * @requires fs
+ * @requires osa-imessage
+ */
+
 const fs = require('fs')
 const imessage = require('./osa-imessage')
 
-const message = process.argv[2] || './attachment.gif'
-const contactsPath = process.argv[3] || './contacts.txt'
+const contactsPath = process.argv[2] || './contacts.txt'
 
 /**
  * get contacts
@@ -17,6 +38,40 @@ const getContacts = filePath => {
   return contacts
 }
 
+/**
+ * retry message send
+ */
+const sendMessage = async ({
+  contact,
+  message,
+  isFile = false,
+  retries = 1,
+  retryCount = 0,
+}) => {
+  try {
+    await imessage.send({
+      handle: contact,
+      message,
+      isFile,
+    })
+  } catch (error) {
+    if (retryCount < retries) {
+      console.log(`${contact} failed. retrying...`)
+      retryCount++
+      await sendMessage({ contact, message, isFile, retries, retryCount })
+    } else {
+      throw error
+    }
+  }
+}
+
+/**
+ * return list of unique items
+ * @param {string[]} list
+ */
+const uniqueArr = list =>
+  list.filter((item, index, self) => self.indexOf(item) === index)
+
 ;(async () => {
   // retrieve contacts
   const contacts = getContacts(contactsPath)
@@ -24,20 +79,21 @@ const getContacts = filePath => {
 
   // send message per contact
   for (let contact of contacts) {
-    // phone number, email, or group chat id
-    try {
-      await imessage.send({
-        handle: contact,
-        message: message,
-        isFile: true,
-      })
-    } catch (error) {
-      invalidContacts.push(contact)
+    for (let msg of MESSAGES_TO_SEND) {
+      try {
+        await sendMessage({ contact, ...msg })
+      } catch (error) {
+        console.log('invalid contact', contact)
+        invalidContacts.push(contact)
+      }
     }
   }
 
   // display any errors
   if (invalidContacts.length) {
+    // only unique
+    invalidContacts = uniqueArr(invalidContacts)
+
     fs.writeFileSync('./invalidContacts.txt', invalidContacts.join('\n'))
     console.error('INVALID CONTACTS', invalidContacts)
   }
--- readme.md
+++ readme.md
@@ -9,12 +9,14 @@ At the project root folder:
 
 ## Usage
 
-At the root of the project foler add a phone number per line in the _contacts.txt_. Also replace the _attachement.gif_ with your own otherwise if using a different attachement then make sure to mention the path when initialising.
+At the root of the project folder add a phone number per line in the _contacts.txt_. Also replace the _attachement.gif_ with your own otherwise if using a different attachement then make sure to mention the path when initialising.
 
 At the project root folder:
 `node ./app.js` or `npm start`
 
-Optionally you can specify message **attachment** location and **contacts** list location on start.
-`node ./app.js ./attachment.gif ./contacts.txt`
+Optionally you can specify your **contacts** list location on start.
+`node ./app.js ./contacts.txt`
+
+The top of the script `app.js` is an array of message objects which you can change to suit your needs.
 
 If you have invalid contacts a **invalidContacts.txt** file will be generated at the root.
